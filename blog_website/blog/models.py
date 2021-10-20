a6032fa66f6aafe8f84058bcbb6e751f0c34ac7b
tran-haison <haison250199@gmail.com>
2021-10-20 23:25:02 +0700

    Create Post model and new users app

--- blog_website/blog/models.py
+++ blog_website/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Class "Post" is defined as table "Post" in db, extends Model class
+class Post(models.Model):
+    # Define columns
+
+    # Title
+    title = models.CharField(max_length=100)
+
+    # Content
+    content = models.TextField()
+
+    # Date posted (automatically created by default)
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    # Author
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 19th, 2021',
-    },
-    {
-        'author': 'Hai Son',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'November 20th, 2032',
-    },
-]
-
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'October 19th, 2021',
+#     },
+#     {
+#         'author': 'Hai Son',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'November 20th, 2032',
+#     },
+# ]
 
 def home(request):
-    # Data needs to be passed
+    # Dummy data needs to be passed
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     # Return a http response
